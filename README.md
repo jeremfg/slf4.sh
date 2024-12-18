a5a221c66f69b54872a5651b24df1db7dbdb1e47
Jérémie Faucher-Goulet <jeremiefauchergoulet@hotmail.com>
2024-12-18 15:26:14 -0500

    Release 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # slf4sh
+
 Simple Logging Facade for Shell
+
+## Install
+
+## Features
+
+Provides the following functions:
+
+```bash
+logTest()
+logTrace()
+logDebug()
+logInfo()
+logWarn()
+logError()
+logFatal()
+
+logSetLevel()
+```
+
+## Setup for developers
+
+Once you've checked out this repository call the following:
+
+```bash
+./tool/setup
+```
+
+### Release
+
+To create a release, call
+
+```bash
+./tool_release <version>
+```
+
+Where `<version>` is a semantic version 2.0.0 version number
+
+## TODO
+
+[ ] Document using shdoc: <https://github.com/reconquest/shdoc>
+[ ] Package releases with BPKG: <https://bpkg.sh/>
+
+## Features being considered
+
+[ ] Add color per log level
+[ ] Add PÎD (Process ID) to the printed message metadata
---
+++ src/slf4.sh
@@ -0,0 +1,189 @@
+# shellcheck shell=bash
+# SPDX-License-Identifier: MIT
+#
+# Source this into any shell script you wish to add logging support.
+# Inspired From: https://serverfault.com/a/103569
+#
+
+if [[ -z ${GUARD_SLF4SH_SH} ]]; then
+  GUARD_SLF4SH_SH=1
+else
+  return
+fi
+
+# shellcheck disable=SC2034  # Unused variables left documentation purpose
+SLF4SH_VERSION="1.0.0"
+
+# LEVEL_ALL appears unused. Verify use (or export if used externally).
+# LEVEL_OFF appears unused. Verify use (or export if used externally).
+# shellcheck disable=2034
+
+LEVEL_ALL=0   # Enable all possible logs.
+LEVEL_TEST=1  # Only used by developers to temporarily add logs that should never get commited.
+LEVEL_TRACE=2 # Low level tracing of very detailed specifc information.
+LEVEL_DEBUG=3 # Might allow to investigate and resolve a bug.
+LEVEL_INFO=4  # Default level. High level indications of the paths the code took.
+LEVEL_WARN=5  # Unusual behavior that don't cause an issue to the current execution.
+LEVEL_ERROR=6 # A recoverable error occured. The program is still executing properly but the user is not getting the desired outcome.
+LEVEL_FATAL=7 # Unrecoverable error. Program is exiting immediately for safety as it wasn't designed to continue after this.
+# shellcheck disable=SC2034  # Unused variables left for readability
+LEVEL_OFF=8 # Turns of all logs
+
+# If no level is configured, start at INFO
+if [[ -z "${LOG_LEVEL}" ]]; then
+  LOG_LEVEL=${LEVEL_INFO}
+fi
+
+# By default do not print logs on the console but only in the log file
+if [[ -z "${LOG_CONSOLE}" ]]; then
+  LOG_CONSOLE=0
+fi
+
+# Set the log level
+#
+# @parms[in] #1: The logging level to be used from now on, using one of the LEVEL_* values
+logSetLevel() {
+  LOG_LEVEL="$1"
+}
+
+logFatal() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_FATAL} ]]; then
+    log "FATAL " "$@"
+  fi
+  exit 1
+}
+
+logError() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_ERROR} ]]; then
+    log "ERROR " "$@"
+  fi
+}
+
+logWarn() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_WARN} ]]; then
+    log "WARN  " "$@"
+  fi
+}
+
+logInfo() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_INFO} ]]; then
+    log "INFO  " "$@"
+  fi
+}
+
+logDebug() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_DEBUG} ]]; then
+    log "DEBUG " "$@"
+  fi
+}
+
+logTrace() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_TRACE} ]]; then
+    log "TRACE " "$@"
+  fi
+}
+
+logTest() {
+  if [[ "${LOG_LEVEL}" -le ${LEVEL_TEST} ]]; then
+    log "TEST  " "$@"
+  fi
+}
+
+log() {
+  local date=0
+  local time=0
+  local full=0
+  date=$(date +%F)
+  time=$(date +%H:%M:%S)
+  full="${date} ${time}"
+
+  local level="$1"
+  shift
+
+  local out_line
+  if [[ -z "$*" ]]; then
+    local line
+    out_line="(message from pipe follows below)"
+    while IFS= read -r line; do
+      out_line+="\n${line}"
+    done
+  else
+    out_line="$*"
+  fi
+
+  if [[ "${LOG_CONSOLE}" == 1 ]]; then
+    echo -e "${full} ${level}${out_line}"
+  else
+    echo -e "${full} ${level}${out_line}" >>"${SL_LOGFILE}"
+  fi
+}
+
+sl_init() {
+  local start_date start_time start logDir
+  start_date="$(date +%F)"
+  start_time="$(date +%H%M%S)"
+  start="${start_date}_${start_time}"
+  logDir="${SL_CWD}"
+
+  # If git is supported, try to find parent repository root
+  if command -v git &>/dev/null; then
+    local curDir
+    local res
+    curDir="$(cd "${logDir}" && git rev-parse --show-toplevel &>/dev/null)"
+    res=$?
+    # Walk-up the tree to exit any potential git repository
+    while [[ ${res} -eq 0 ]]; do
+      # Check if parent is inside a git repository
+      if cd "${curDir}/.." && git rev-parse --is-inside-work-tree &>/dev/null; then
+        curDir="$(cd "${curDir}/.." && git rev-parse --show-toplevel &>/dev/null)"
+        curDir="$(realpath "${curDir}")"
+      else
+        logDir="${curDir}"
+        break # We've escaped outside of the git repo
+      fi
+    done
+    # Make sure CWD was not moidified
+    cd "${SL_CWD}" || return 1
+  fi
+
+  # Path Configuration
+  declare -g SL_LOGFILE
+  SL_LOGFILE="${logDir}/.log/${SL_ME%.*}_${start}.log"
+
+  # Setup logging
+  mkdir -p "$(dirname "${SL_LOGFILE}")" # Create log directory
+  exec 3>&1 4>&2                        # Backup old descriptors
+  trap 'exec 2>&4 1>&3' 0 1 2 3         # Restore in case of signals
+  # shellcheck disable=2312
+  exec &> >(tee -a "${SL_LOGFILE}") # Redirect output
+}
+
+###########################
+###### Startup logic ######
+###########################
+SL_CWD=$(pwd)
+SL_ME="$(basename "${BASH_SOURCE[0]}")"
+
+# Get root directory of the project
+# https://stackoverflow.com/a/246128
+SL_SOURCE=${BASH_SOURCE[0]}
+while [[ -L "${SL_SOURCE}" ]]; do # resolve $SL_SOURCE until the file is no longer a symlink
+  SL_ROOT=$(cd -P "$(dirname "${SL_SOURCE}")" >/dev/null 2>&1 && pwd)
+  SL_SOURCE=$(readlink "${SL_SOURCE}")
+  [[ ${SL_SOURCE} != /* ]] && SL_SOURCE=${SL_ROOT}/${SL_SOURCE} # if $SL_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SL_ROOT=$(cd -P "$(dirname "${SL_SOURCE}")" >/dev/null 2>&1 && pwd)
+SL_ROOT=$(realpath "${SL_ROOT}/..")
+
+if [[ -p /dev/stdin ]] && [[ -z ${BASH_SOURCE[0]} ]]; then
+  # This script was piped
+  echo "ERROR: This script cannot be piped"
+  exit 1
+elif [[ ${BASH_SOURCE[0]} != "${0}" ]]; then
+  # This script was sourced
+  sl_init
+else
+  # This script was executed
+  echo "ERROR: This script cannot be executed"
+  exit 1
+fi
